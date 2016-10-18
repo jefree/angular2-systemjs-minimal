9807bef6164ba899a3d949a69be794b8af107627
jefree <jeffersongarzonm@gmail.com>
2016-10-17 20:00:57 -0500

    initial setup

---
+++ app/component/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>
+      <h1> Truly Wonderful Angular 2 is </h1>
+    </div>`
+})
+export class AppComponent {
+  title = "la primera cosa que se me vino a la cabeza";
+}
+
